b22fde8899e0ff2def3397ab0d4bed1f027c79c7
abhishek <avishekchaturvedi0@gmail.com>
2024-12-28 03:40:02 +0530

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://api.isbec.com/api'
+    ? 'https://isbec-backend2.onrender.com'
     : 'http://localhost:8081/api';
 
 const api = axios.create({
